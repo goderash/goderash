ffc7da08a01529a0b52699099a949cbb44e2b6e1
Atnabon <atnabond@gmail.com>
2026-05-05 22:34:43 +0300

    feat: update __all__ exports to include WebhookEndpoint and EmailVerificationToken

--- python/core/src/goderash_core/models/__init__.py
+++ python/core/src/goderash_core/models/__init__.py
@@ -1,6 +1,18 @@
 """ORM models."""
 
+from .auth_tokens import EmailVerificationToken, Invite, PasswordResetToken
 from .event import EventRow
-from .tenant import ApiKey, Tenant
+from .tenant import ApiKey, Tenant, WebhookEndpoint
+from .user import Membership, User
 
-__all__ = ["ApiKey", "EventRow", "Tenant"]
+__all__ = [
+    "ApiKey",
+    "EmailVerificationToken",
+    "EventRow",
+    "Invite",
+    "Membership",
+    "PasswordResetToken",
+    "Tenant",
+    "User",
+    "WebhookEndpoint",
+]
